250a03ee0a4c7390bb1558bd9a517b8213c7d50f
army-17 <j2665138@naver.com>
2022-12-09 14:53:50 +0900

    file

---
+++ wadizIT/src/main/java/com/jsframe/wadizit/WadizItApplication.java
@@ -0,0 +1,13 @@
+package com.jsframe.wadizit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WadizItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WadizItApplication.class, args);
+	}
+
+}
