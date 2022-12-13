2f8ab2d13b8f495f916dc86831ec1982947758fa
army-17 <j2665138@naver.com>
2022-12-13 14:40:56 +0900

    boardcomment

---
+++ wadizIT/src/main/java/com/jsframe/wadizit/service/FundingCommentService.java
@@ -0,0 +1,14 @@
+package com.jsframe.wadizit.service;
+
+
+import lombok.extern.java.Log;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Service
+@Log
+public class FundingCommentService {
+
+
+}
