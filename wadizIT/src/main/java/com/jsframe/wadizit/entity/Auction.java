4b83c86b35b1a5526926738ce5ce6b7f4ae03052
ICIA20220729T5AR <ICIA20220729T5AR@gmail.com>
2022-12-13 13:46:35 +0900

    채팅 서비스 추가

--- wadizIT/src/main/java/com/jsframe/wadizit/entity/Auction.java
+++ wadizIT/src/main/java/com/jsframe/wadizit/entity/Auction.java
@@ -11,16 +11,20 @@ public class Auction {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long auctionNum;
 
+    // 경매 제목
     @Column(nullable = false, length = 100)
     private String title;
 
+    // 경매 상태(0: 진행, 1: 종료)
     @Column(nullable = false)
     @ColumnDefault("-1")
     private long status;
 
+    // 시작가
     @Column(nullable = false)
     private long startPrice;
 
+    // 현재가
     @Column(nullable = false)
     private long currentPrice;
 }
