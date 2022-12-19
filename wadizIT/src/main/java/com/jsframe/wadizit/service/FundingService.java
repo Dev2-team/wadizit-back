e3a6bcd9b4a8400e9922c482919bbe262fb40353
hee1021 <wor1380@naver.com>
2022-12-19 10:56:52 +0900

    mo2

--- wadizIT/src/main/java/com/jsframe/wadizit/service/FundingService.java
+++ wadizIT/src/main/java/com/jsframe/wadizit/service/FundingService.java
@@ -1,39 +1,36 @@
 package com.jsframe.wadizit.service;
 
 import com.jsframe.wadizit.entity.Funding;
-import com.jsframe.wadizit.entity.Member;
 import com.jsframe.wadizit.repository.FundingRepository;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
 @Log
+@Service
 public class FundingService {
 
     @Autowired
     private FundingRepository fRepo;
 
     //펀딩 게시글 생성
-    public String create(Funding funding, Member member) {
+    public String create(Funding funding) {
         log.info("create()");
         String msg = null;
 
-        log.info(""+funding.getFundingNum());
+        log.info("" + funding.getFundingNum());
         //log.info(funding.getContent());
         log.info(funding.getTitle());
-        funding.setMemberNum(member);
 
-        try{
+        try {
             fRepo.save(funding);
             msg = "펀딩 등록 성공";
-        } catch (Exception e){
+        } catch (Exception e) {
             log.info(e.getMessage());
             msg = "펀딩 등록 실패";
         }
 
         return msg;
-
     }
 
     //펀딩 게시글 읽기
@@ -41,9 +38,8 @@ public class FundingService {
         log.info("getFunding()");
 
         Funding fund = fRepo.findById(fundingNum).get();
-        log.info("출력 : "+ fund.getFundingNum());
+        log.info("출력 : " + fund.getFundingNum());
         return fund;
-
     }
 
     //펀딩 게시글 삭제
@@ -51,14 +47,13 @@ public class FundingService {
         log.info("deleteFunding()");
         String msg = null;
 
-        try{
+        try {
             fRepo.deleteById(fundingNum);
             msg = "삭제 성공";
-        } catch (Exception e){
+        } catch (Exception e) {
             msg = "삭제 실패";
         }
         return msg;
-
     }
 
     //펀딩 게시글 리스트
@@ -70,39 +65,19 @@ public class FundingService {
     }
 
     //펀딩 게시글 수정
-    public String update(Funding funding, Long fundingNum, Member member) {
+    public String update(Funding funding, Long fundingNum) {
         log.info("update()");
         String msg = null;
 
-        //로그인한 사람의 memberNum
-        long loginPerson = member.getMemberNum();
-        //펀드 작성자의 memberNum
-        Funding fund3 = fRepo.findById(fundingNum).get();
-        long fundWriter = (fund3.getMemberNum()).getMemberNum();
-
-        log.info(""+fund3.getMemberNum().getMemberNum());
         Funding funding3 = fRepo.findById(fundingNum).get();
         funding3.setTitle(funding.getTitle());
 
-        if ( loginPerson == fundWriter) {
-
-            fund3.setTitle(funding.getTitle());
-            fund3.setCategory(funding.getCategory());
-            fund3.setStartDate(funding.getStartDate());
-            fund3.setEndDate(funding.getEndDate());
-
-
-            try {
-                fRepo.save(funding3);
-                msg = "수정 성공";
-            } catch (Exception e) {
-                msg = "수정 실패";
-            }
+        try {
+            fRepo.save(funding3);
+            msg = "수정 성공";
+        } catch (Exception e) {
+            msg = "수정 실패";
         }
-        else{
-                msg = "작성자만 수정 가능합니다";
-            }
-            return msg;
-
+        return msg;
     }
 }
