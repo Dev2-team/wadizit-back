ad032570b2d025f30607e0f8645135879a567f00
samsaekee <esk2169@naver.com>
2023-01-10 13:42:57 +0900

    payment 수정완

--- wadizIT/src/main/java/com/jsframe/wadizit/service/PaymentService.java
+++ wadizIT/src/main/java/com/jsframe/wadizit/service/PaymentService.java
@@ -7,11 +7,8 @@ import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.http.HttpSession;
-import java.sql.Date;
 import java.sql.Timestamp;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.util.List;
 
 @Service
 @Log
@@ -19,18 +16,24 @@ public class PaymentService {
     @Autowired
     private PaymentRepository pRepo;
 
-    public void save(String oNum, String date, Member member) {
+    public void save(String oNum, String oName, String date, Member member) {
+        log.info("save()");
+
         Payment payment = new Payment();
         Timestamp tp = Timestamp.valueOf(date);
 
         try {
             payment.setOrderNum(oNum);
+            payment.setOrderName(oName);
             payment.setMemberNum(member);
             payment.setDate(tp);
-
             pRepo.save(payment);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public List<Payment> getPayment(Member member) {
+        return pRepo.findByMemberNum(member);
     }
 }
