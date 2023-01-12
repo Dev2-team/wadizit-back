8315e1238c69f5ffcd450ba8b46a133189a4e15d
ICIA20220729T5AR <ICIA20220729T5AR@gmail.com>
2023-01-12 13:19:10 +0900

    엔티티 추가 및 수정

--- wadizIT/src/main/java/com/jsframe/wadizit/repository/TokenRepository.java
+++ wadizIT/src/main/java/com/jsframe/wadizit/repository/TokenRepository.java
@@ -1,6 +1,5 @@
 package com.jsframe.wadizit.repository;
 
-import com.jsframe.wadizit.entity.Funding;
 import com.jsframe.wadizit.entity.Token;
 import org.springframework.data.repository.CrudRepository;
 
@@ -8,6 +7,4 @@ import java.util.List;
 
 public interface TokenRepository extends CrudRepository<Token, Long> {
     List<Token> findAll();
-
-    Token findByFundingNum(Funding fundingNum);
 }
