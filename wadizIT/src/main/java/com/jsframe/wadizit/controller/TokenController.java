c25dd1a4ce414382dc5dddec773765452d9c61ac
ICIA20220729T5AR <ICIA20220729T5AR@gmail.com>
2023-01-13 15:02:07 +0900

    보유 토큰 리스트 반환 기능 추가

--- wadizIT/src/main/java/com/jsframe/wadizit/controller/TokenController.java
+++ wadizIT/src/main/java/com/jsframe/wadizit/controller/TokenController.java
@@ -1,6 +1,11 @@
 package com.jsframe.wadizit.controller;
 
+import com.jsframe.wadizit.dto.TokenPossessionDto;
+import com.jsframe.wadizit.entity.Member;
+import com.jsframe.wadizit.entity.MemberTokenID;
 import com.jsframe.wadizit.entity.Token;
+import com.jsframe.wadizit.entity.TokenPossession;
+import com.jsframe.wadizit.repository.TokenPossessionRepository;
 import com.jsframe.wadizit.repository.TokenRepository;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +13,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,6 +24,8 @@ import java.util.Optional;
 public class TokenController {
     @Autowired
     private TokenRepository tokenRepo;
+    @Autowired
+    private TokenPossessionRepository tokenPossessionRepo;
     private HttpStatus respStatus;
 
     @PostMapping("")
@@ -52,5 +61,21 @@ public class TokenController {
         if (tokenList.size() == 0) respStatus = HttpStatus.NO_CONTENT;
         else respStatus = HttpStatus.OK;
         return ResponseEntity.status(respStatus).body(tokenList);
+    }
+
+    @GetMapping("/list/possession")
+    public ResponseEntity getPossessionList(HttpSession session) {
+        Member member = (Member)session.getAttribute("mem");
+        List<TokenPossession> tpList = tokenPossessionRepo.findAllByMemberNum(member.getMemberNum());
+        List<TokenPossessionDto> tpDtoList = new ArrayList<>();
+        for (int i=0; i<tpList.size(); i++) {
+            Token token = tokenRepo.findById(tpList.get(i).getTokenNum()).get();
+            TokenPossessionDto tpDto = new TokenPossessionDto();
+            tpDto.setTokenNum(token.getTokenNum());
+            tpDto.setTokenSymbol(token.getName());
+            tpDto.setTokenAmount(tpList.get(i).getAmount());
+            tpDtoList.add(tpDto);
+        }
+        return ResponseEntity.status(200).body(tpDtoList);
     }
 }
