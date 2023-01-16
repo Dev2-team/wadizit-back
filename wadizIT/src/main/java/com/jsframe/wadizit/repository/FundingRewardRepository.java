38959abacdc780e4b095966e553f66d48c1811ce
ICIA20220729T5AR <ICIA20220729T5AR@gmail.com>
2023-01-16 14:18:31 +0900

    펀딩 리워드 관련 삭제

--- wadizIT/src/main/java/com/jsframe/wadizit/repository/FundingRewardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jsframe.wadizit.repository;
-
-import com.jsframe.wadizit.entity.Funding;
-import com.jsframe.wadizit.entity.Reward;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FundingRewardRepository extends CrudRepository<Reward, Long> {
-    Iterable<Reward> findByFundingNum(Funding fundingData);
-}
